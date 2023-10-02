d6eb543907f7cb4d5b11a63d05333c07c9ef8a18
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r10s1.42madrid.com>
2023-10-02 18:39:07 +0200

    .h added

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,17 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:37:28 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/02 16:46:38 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/02 17:50:01 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdlib.h>
+# define BUFFER_SIZE 1024
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
 
 # endif
