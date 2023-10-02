7a2f26f3620d6fd8b473834acb0c61c56ee3139b
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r10s1.42madrid.com>
2023-10-02 18:38:45 +0200

    .c added

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,43 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:37:41 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/02 16:46:21 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/02 18:35:55 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char *ft_get_line(int fd, char *str)
+{
+	int	line;
+
+	line = 1;
+	while (line != '\0')
+	{
+		line = read(fd, str, BUFFER_SIZE);
+		if (line == -1)
+			return (NULL);
+		if (line == 0)
+			break;
+		
+	}
+	
+	
+}
+
+
 char *get_next_line(int fd)
 {
-    if (fd < 0)
+	char	*str;
+	char	*line;
+	char	*res;
+	
+    if (fd < 0 || BUFFER_SIZE < 0)
 		return (NULL);
+	str = (char * )malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!str)
+		return (NULL);
+	line = ft_get_line(fd, str);
+	
+	return (res);
 }
