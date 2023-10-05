04c586413364ddfb8f935264e39ec0ccb86a9146
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r10s2.42madrid.com>
2023-10-05 11:18:35 +0200

    .h updated

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:37:28 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/02 17:50:01 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/05 11:13:57 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,8 @@
 # include <stdio.h>
 
 char    *get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
 
 # endif
