f6f6164915f0c52b745601e8f546e8a5cd77d78a
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r10s2.42madrid.com>
2023-10-05 11:51:52 +0200

    add get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:37:41 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/05 11:18:19 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/05 11:50:03 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,37 @@ char	*ft_read_line(int fd, char *buff)
 {
 	int		reader;
 	char	*str_aux;
-	char	*result;
 
 	reader = 1;
-	while (reader != 0)
+	str_aux = ft_strdup("");
+	while (ft_strchr(buff, '\n') && (reader != 0))
 	{
 		reader = read(fd, buff, BUFFER_SIZE);
 		if (reader == -1)
 			return (NULL);
 		buff[reader] = '\0';
-		result = ft_strjoin(str_aux, buff);
-		if(ft_strchr(buff, '\n'))
-			break;
+		str_aux = ft_strjoin(str_aux, buff);
+		free(buff);
 	}
-	return (result);
+	return (str_aux);
 }
-
 
 char	*get_next_line(int fd)
 {
 	char		*str;
 	static char	*buff;
-	
-    if (fd < 0 || BUFFER_SIZE < 0)
+
+	if (fd < 0 || BUFFER_SIZE < 0)
 		return (NULL);
-	buff = (char * )malloc(sizeof(char) * BUFFER_SIZE + 1);
+	buff = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buff)
 		return (NULL);
 	buff = ft_read_line(fd, buff);
-
+	return (buff);
 }
+
+/*
+1. Añadir protecciones al str_join
+2. Meter el memcpy
+3. Función los trozos que sobran
+*/
