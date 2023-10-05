46065d790287080b90a53a05654b592bd4009ab6
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r10s2.42madrid.com>
2023-10-05 12:46:21 +0200

    .h updated

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:37:28 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/05 11:48:32 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/05 12:25:57 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 4
+// # define BUFFER_SIZE 4
 
 # include <stdlib.h>
 # include <unistd.h>
@@ -24,5 +24,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
