a25d5b5af4e7d8a148f9b5525924e302481a15f4
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r10s2.42madrid.com>
2023-10-05 11:52:41 +0200

    utils updated

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:37:31 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/02 16:45:10 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/05 11:49:33 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
 	res = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (res == NULL)
 		return (NULL);
@@ -71,4 +73,33 @@ char	*ft_strchr(const char *s, int c)
 	if ((unsigned char)c == 0)
 		return ((char *)ptr);
 	return (NULL);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char		*d;
+	const char	*s;
+	size_t		i;
+
+	i = 0;
+	d = dst;
+	s = src;
+	if (!dst && !src)
+		return (dst);
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dst);
 }
