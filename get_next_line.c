2c25ce8ef4c75955297054e006f59b571849e3c1
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r13s3.42madrid.com>
2023-10-06 15:12:13 +0200

    .c updated

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:37:41 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/05 12:50:24 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/06 15:02:22 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,24 @@ char	*ft_read_line(int fd, char *buff)
 {
 	int		reader;
 	char	*str_aux;
+	char	*temp;
 
 	reader = 1;
 	str_aux = ft_strdup("");
-	while (ft_strchr(buff, '\n') && (reader != 0))
+	while (!(ft_strchr(buff, '\n')) && (reader != 0))
 	{
 		reader = read(fd, buff, BUFFER_SIZE);
 		if (reader == -1)
+		{
+			free(str_aux);
 			return (NULL);
+		}		
 		buff[reader] = '\0';
-		str_aux = ft_strjoin(str_aux, buff);
-		free(buff);
+		temp = ft_strjoin(str_aux, buff);
+		free(str_aux);
+		str_aux = temp;
 	}
+	free(buff);
 	return (str_aux);
 }
 
@@ -39,14 +45,13 @@ char	*ft_get_lines(char *str)
 	i = 0;
 	if (!str)
 		return (NULL);
-	while (str[i] || str[i] != '\n')
+	while (str[i] && str[i] != '\n')
 		i++;
-	res = ft_substr(str, i + 1, ft_strlen(str) - i);
-	if (*res == '\0')
-	{
-		free(res);
-		res = NULL;
-	}
+	if(str[i] == '\0')
+		return (0);
+	res = ft_substr(str, i + 1, ft_strlen(str) - i - 1);
+	if (!res)
+		return (NULL);
 	return (res);
 }
 
@@ -55,23 +60,18 @@ char	*get_next_line(int fd)
 	char		*str;
 	static char	*buff;
 
-	if (fd < 0 || BUFFER_SIZE < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buff = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buff)
-		return (NULL);
+	{
+		buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!buff)
+			return (NULL);
+	}	
 	buff = ft_read_line(fd, buff);
 	if (!buff)
 		return (NULL);
-	free(buff);
-	buff = NULL;
 	str = ft_get_lines(buff);
-	return (buff);
+	return (str);
 }
 
-/*
-1. Añadir protecciones al str_join
-2. Meter el memcpy
-TODO:
-3. Función los trozos que sobran
-*/
