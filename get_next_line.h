c0ac870d534cbbe1ffab311b97562f88e87c1950
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r17s3.42madrid.com>
2023-10-09 12:10:28 +0200

    .h udated functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:37:28 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/05 16:51:13 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/09 11:16:49 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@
 # include <stdio.h>
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_print_line(char *str);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *str);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_new_static(char *aux);
 
 #endif
