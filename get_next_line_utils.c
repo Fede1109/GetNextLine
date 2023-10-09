692a08fdf8cabe26237ef6ad3d983808191da700
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r17s3.42madrid.com>
2023-10-09 12:10:43 +0200

    utills udated

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,109 +6,123 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:37:31 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/05 16:51:09 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/09 12:06:33 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *str)
+char	*ft_strjoin(char *s1, char *s2)
 {
-	size_t	i;
+	char	*str;
+	int		i;
+	int		j;
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*res;
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
+	i = -1;
+	j = -1;
 	if (!s1 || !s2)
-		return (NULL);
-	res = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (res == NULL)
-		return (NULL);
-	while (s1[i])
 	{
-		res[i] = s1[i];
-		i++;
+		s1 = malloc(sizeof(char) * 1);
+		if (!s1 || !s2)
+			return (NULL);
+		s1[0] = '\0';
 	}
-	while (s2[j])
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str)
 	{
-		res[i] = s2[j];
-		i++;
-		j++;
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
+		while (s2[++j] != '\0')
+			str[i + j] = s2[j];
+		str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
 	}
-	res[i] = '\0';
-	return (res);
+	free(s1);
+	return (str);
 }
 
-char	*ft_strdup(const char *s1)
+char	*ft_print_line(char *str)
 {
-	char	*ptr;
-	size_t	size_len;
-	size_t	i;
+	int		i;
+	char	*ret;
 
 	i = 0;
-	size_len = ft_strlen(s1);
-	ptr = (char *)malloc(size_len + 1);
-	if (ptr == NULL)
+	if (!str[i])
 		return (NULL);
-	while (i < size_len)
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	ret = malloc(sizeof(char) * i + 1);
+	if (!ret)			
+		return (NULL);
+	i = -1;
+	while (str[++i] && str[i] != '\n')
+		ret[i] = str[i];
+	if (str[i] == '\n')
 	{
-		ptr[i] = s1[i];
+		ret[i] = str[i];
 		i++;
 	}
-	ptr[size_len] = '\0';
-	return (ptr);
+	ret[i] = '\0';
+	return (ret);
 }
 
 char	*ft_strchr(const char *s, int c)
 {
-	char	*ptr;
+	int	i;
 
-	ptr = NULL;
-	ptr = (char *)s;
-	while (*ptr)
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i])
 	{
-		if (*ptr == (unsigned char)c)
-		{
-			return (ptr);
-		}
-		ptr++;
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
 	}
-	if ((unsigned char)c == 0)
-		return ((char *)ptr);
+	if ((char)c == '\0')
+		return ((char *)&s[i]);
 	return (NULL);
 }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+size_t	ft_strlen(const char *str)
 {
-	size_t	i;
-	size_t	j;
-	char	*str;
+	size_t	counter;
 
-	str = (char *)malloc(sizeof(*s) * (len + 1));
-	if (str == 0)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (s[i])
+	counter = 0;
+	while (*str != '\0')
 	{
-		if (i >= start && j < len)
-		{
-			str[j] = s[i];
-			j++;
-		}
-		i++;
+		counter++;
+		str++;
 	}
-	str[j] = 0;
-	return (str);
+	return (counter);
+}
+
+char	*ft_new_static(char *aux)
+{
+	int		i;
+	int		j;
+	char	*ret;
+
+	i = 0;
+	while (aux[i] && aux[i] != '\n')
+		i++;
+	if (!aux[i])
+	{
+		free(aux);
+		return (NULL);
+	}
+	ret = malloc(sizeof(char) * (ft_strlen(aux) - i + 1));
+	if (!ret)
+	{
+		free(aux);
+		return (NULL);
+	}
+	i++;
+	j = 0;	
+	while (aux[i] != '\0')
+		ret[j++] = aux[i++];
+	ret[j] = '\0';
+	free(aux);
+	return (ret);
 }
