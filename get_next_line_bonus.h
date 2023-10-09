4b579dcd28dbfaa7eb611777c9b409df6aec0107
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r17s3.42madrid.com>
2023-10-09 13:59:36 +0200

    .h bons added

---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 14:37:28 by fdiaz-gu          #+#    #+#             */
+/*   Updated: 2023/10/09 13:52:42 by fdiaz-gu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+// # define BUFFER_SIZE 4
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_extract_line(char *str);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+char	*ft_new_static(char *aux);
+
+#endif
