d7b961f0705ecb1d7151e851f19402557a32cbd3
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r10s2.42madrid.com>
2023-10-10 12:14:26 +0200

    normi

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:37:41 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/10 11:57:35 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/10 12:10:32 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,14 @@ char	*ft_write_in_aux(int fd, char *aux)
 		check = read(fd, buffer, BUFFER_SIZE);
 		if (check == -1)
 		{
-			free(buffer);			
+			free(buffer);
+			free(aux);
 			return (NULL);
 		}
 		buffer[check] = '\0';
 		aux = ft_strjoin(aux, buffer);
 	}
-	free(buffer);	
+	free(buffer);
 	return (aux);
 }
 
@@ -48,24 +49,22 @@ char	*get_next_line(int fd)
 		return (NULL);
 	line = ft_extract_line(aux);
 	aux = ft_new_static(aux);
-	// system("leaks a.out");
 	return (line);
 }
 
-int main(void)
-{
-	int fd1;
-	char	*str;
-	
-	fd1 = open("hola.txt", O_RDONLY);
-	str = get_next_line(fd1);
-	printf("%s\n", str);
-	str = get_next_line(fd1);
-	printf("%s\n", str);
-	str = get_next_line(fd1);
-	printf("%s\n", str);
-	str = get_next_line(fd1);
-	printf("%s\n", str);
-	close(fd1);
-	return (0);
-}
+// int	main(void)
+// {
+// 	int fd1;
+// 	char	*str;
+// 	fd1 = open("hola.txt", O_RDONLY);
+// 	str = get_next_line(fd1);
+// 	printf("%s\n", str);
+// 	str = get_next_line(fd1);
+// 	printf("%s\n", str);
+// 	str = get_next_line(fd1);
+// 	printf("%s\n", str);
+// 	str = get_next_line(fd1);
+// 	printf("%s\n", str);
+// 	close(fd1);
+// 	return (0);
+// }
