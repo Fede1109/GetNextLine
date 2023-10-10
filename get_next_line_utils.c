5999aca0fbf3d8ab9cd0a8c08843c0100ef5cecd
Federico Jose Diaz Guanoluiza <fdiaz-gu@c2r10s2.42madrid.com>
2023-10-10 11:59:18 +0200

    updated utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:37:31 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/10/09 13:48:40 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/10/10 11:50:34 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,19 @@ char	*ft_strjoin(char *s1, char *s2)
 	j = -1;
 	if (!s1 || !s2)
 	{
-		s1 = malloc(sizeof(char) * 1);
+		s1 = malloc(sizeof(char) + 1);
 		if (!s1 || !s2)
 			return (NULL);
 		s1[0] = '\0';
 	}
-	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (str)
 	{
 		while (s1[++i] != '\0')
 			str[i] = s1[i];
 		while (s2[++j] != '\0')
 			str[i + j] = s2[j];
-		str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+		str[i + j] = '\0';
 	}
 	free(s1);
 	return (str);
@@ -94,7 +94,7 @@ size_t	ft_strlen(const char *str)
 	{
 		counter++;
 		str++;
-	}
+	}	
 	return (counter);
 }
 
@@ -123,6 +123,6 @@ char	*ft_new_static(char *aux)
 	while (aux[i] != '\0')
 		ret[j++] = aux[i++];
 	ret[j] = '\0';
-	free(aux);
+	free(aux);	
 	return (ret);
 }
